17a1c37bb8f390152239dcbafef9f4a320a05f1f
Ian <DrilonIan@yahoo.com>
2020-12-25 18:01:47 +0800

    adding function sidebar in event page

--- pages/Event.tsx
+++ pages/Event.tsx
@@ -1,8 +1,20 @@
 import React, { useContext } from "react";
 import { GlobalStateContext } from "../Context/GlobalState";
-import { SubLayout, EventHeader, ListEvents } from "../components";
+import { SubLayout, EventHeader, ListEvents, Sidebar } from "../components";
 const Event: React.FC = () => {
-  const { navbar, dispatch2 } = useContext(GlobalStateContext);
+  const { navbar, dispatch2, dispatch1, toggleSide } = useContext(
+    GlobalStateContext
+  );
+
+  const toggle = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    event.preventDefault();
+
+    dispatch1({
+      type: "toggleSidebar",
+      payload: { toggleSidebar: !toggleSide },
+    });
+  };
+
   const isDark = (event: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
     event.preventDefault();
 
@@ -12,8 +24,11 @@ const Event: React.FC = () => {
     });
   };
 
+  console.log(navbar);
+
   return (
     <SubLayout isDark={(event) => isDark(event)} state={navbar}>
+      <Sidebar toggleSidebar={toggleSide} close={(event) => toggle(event)} />
       <EventHeader />
       <ListEvents />
     </SubLayout>
